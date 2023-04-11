05bcf55f1b08d584932cc4d1cc8c381065cba4d7
Jack Jazrawy-Brown <jackjazb@gmail.com>
2023-04-11 20:48:25 +0100

    Cleanup types

--- src/main.rs
+++ src/main.rs
@@ -64,23 +64,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         Object::new(Vec3::new(12.0, 6.0, -4.0), cube()),
     ];
 
-    let mut counter = 0.0;
+    let mut degs = 0.0;
 
-    let mut last_mouse: (f32, f32) = (0.0, 0.0);
+    let mut last_mouse: (f64, f64) = (0.0, 0.0);
 
     // Keep track of delta time for animation smoothing
     let mut start = SystemTime::now();
     let mut end = SystemTime::now();
     let mut delta: f64;
-    let mut fps = 0.0;
 
     // Main loop
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        // Update timing values
         delta = (end.duration_since(start)?.as_millis() as f64) / 30.0;
-        fps = 1000.0 / (end.duration_since(start)?.as_millis() as f64);
         start = SystemTime::now();
+
         renderer.clear();
-        renderer.write_text("text test", (10, 10));
+
         // Movement control
         if window.is_key_down(Key::W) {
             camera.translate(SPEED * delta, 0.0);
@@ -97,9 +97,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         window.get_mouse_pos(minifb::MouseMode::Clamp).map(|mouse| {
-            let x_offset = (last_mouse.0 - mouse.0) * (LOOK_SPEED * SENSITIVITY * delta) as f32;
-            let y_offset = (mouse.1 - last_mouse.1) * (LOOK_SPEED * SENSITIVITY * delta) as f32;
-            last_mouse = mouse;
+            let x_offset = (last_mouse.0 - mouse.0 as f64) * (LOOK_SPEED * SENSITIVITY * delta);
+            let y_offset = (mouse.1 as f64 - last_mouse.1) * (LOOK_SPEED * SENSITIVITY * delta);
+            last_mouse = (mouse.0 as f64, mouse.1 as f64);
             // camera.rotate(Vec3::new(y_offset as f64, x_offset as f64, 0.0));
         });
 
@@ -117,17 +117,18 @@ fn main() -> Result<(), Box<dyn Error>> {
             camera.rotate(Vec3::new(0.0, -LOOK_SPEED, 0.0).scale(delta));
         }
 
+        renderer.write_text("text test", (10, 10));
+
         // Add some movement to the world
-        counter = counter + deg_to_rad(4.0 * delta);
-        let frame_transform = Mat4::identity().rotate(Y_AXIS, counter / 2.0).scale(2.0);
-        //.translate(Vec3::new(0.0, -0.5 + counter.sin().abs() * 2.0, 0.0));
+        degs = degs + deg_to_rad(4.0 * delta);
+        let per_frame_transform = Mat4::identity().rotate(Y_AXIS, degs / 2.0).scale(2.0);
 
         // Draw each object
         for object in scene_objects.iter() {
             // Send sets of three from each objects vertices to the triangle renderer
             let mut tri_buffer: Vec<Vec3> = vec![];
             for &vertex in object.vertices.iter() {
-                let bounced = frame_transform.transform(vertex);
+                let bounced = per_frame_transform.transform(vertex);
 
                 // Transform each object relative to world space
                 let rel_to_world = Mat4::identity().translate(object.pos).transform(bounced);
@@ -151,9 +152,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
 
-        // to draw a pixel from an image
-        // buffer[i] = rgba_to_u32(img.get_pixel(x as u32, y as u32));
-
         window
             .update_with_buffer(&renderer.buffer, WIDTH, HEIGHT)
             .unwrap();
@@ -161,12 +159,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     Ok(())
 }
-
-// fn rgba_to_u32(pixel: &Rgba<u8>) -> u32 {
-//     let channels = pixel.channels();
-//     let (r, g, b) = (channels[0] as u32, channels[1] as u32, channels[2] as u32);
-//     (r << 16) | (g << 8) | b
-// }
 
 fn deg_to_rad(deg: f64) -> f64 {
     deg * (PI / 180.0)
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,13 +1,9 @@
-use std::collections::{hash_map::Entry, HashMap};
-
 use fontdue::Font;
 
 use crate::Vec3;
 
 const BLACK: u32 = 0x000000;
 const WHITE: u32 = 0xffffff;
-const SKY: u32 = 0x0000ff;
-const GROUND: u32 = 0x00ff00;
 
 pub struct Renderer {
     width: usize,
@@ -18,9 +14,8 @@ pub struct Renderer {
 
 impl Renderer {
     pub fn new(width: usize, height: usize) -> Self {
-        // Read the font data.
+        // Read the font data and parse it into the font type
         let font = include_bytes!("../resources/liberation-mono.ttf") as &[u8];
-        // Parse it into the font type.
         let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
 
         Renderer {
@@ -31,32 +26,24 @@ impl Renderer {
         }
     }
 
-    pub fn write_text(&mut self, text: &str, pos: (u32, u32)) {
+    pub fn write_text(&mut self, text: &str, pos: (usize, usize)) {
         let mut x_offset = pos.0;
-        let space_offset = self.font.rasterize('a', 15.0).0.width;
         for char in text.chars() {
             // Rasterize and get the layout metrics for the letter 'g' at 17px.
-            let (metrics, bitmap) = self.font.rasterize(char, 15.0);
+            let (metrics, bitmap) = self.font.rasterize(char, 11.0);
+
             for y in 0..metrics.height {
                 for x in 0..metrics.width {
                     let char_s = bitmap[x + y * metrics.width];
-                    if char_s > 20 {
-                        self.draw_pixel(
-                            [(x + x_offset as usize) as i64, (y + pos.1 as usize) as i64],
-                            char_s as u32,
-                        );
-                    }
+                    self.draw_pixel(((x + x_offset), (y + pos.1)), char_s as u32);
                 }
             }
-            x_offset = x_offset + space_offset as u32;
+            x_offset = x_offset + metrics.advance_width as usize;
         }
     }
     // Draws a triangle from an array of 3 points.
     pub fn draw_triangle(&mut self, vertices: Vec<Vec3>) {
         let mut vert_index: usize = 0;
-
-        // This holds a map of screen Y to an ordererd vector of pixel X locations
-        let mut row_pixels: HashMap<i64, Vec<i64>> = HashMap::new();
 
         while vert_index < 3 {
             let mut next_vert_index = vert_index + 1;
@@ -81,33 +68,26 @@ impl Renderer {
 
             // Bresenham's line algorithm - info here:
             // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm#Algorithm_for_integer_arithmetic
-            let a = [vec1.x as i64, vec1.y as i64];
-            let b = [vec2.x as i64, vec2.y as i64];
-            self.draw_line(a, b, &mut row_pixels);
+            let a = (vec1.x as isize, vec1.y as isize);
+            let b = (vec2.x as isize, vec2.y as isize);
+            self.draw_line(a, b);
             vert_index = vert_index + 1;
-        }
-
-        for row in row_pixels {
-            let y = row.0;
-            for x in row.1[0]..row.1[row.1.len() - 1] {
-                self.draw_pixel([x, y], WHITE);
-            }
         }
     }
 
-    fn draw_line(&mut self, a: [i64; 2], b: [i64; 2], row_pixels: &mut HashMap<i64, Vec<i64>>) {
-        let dx = (b[0] - a[0]).abs();
-        let dy = -(b[1] - a[1]).abs();
+    fn draw_line(&mut self, a: (isize, isize), b: (isize, isize)) {
+        let dx = (b.0 - a.0).abs();
+        let dy = -(b.1 - a.1).abs();
 
         let sx = {
-            if a[0] < b[0] {
+            if a.0 < b.0 {
                 1
             } else {
                 -1
             }
         };
         let sy = {
-            if a[1] < b[1] {
+            if a.1 < b.1 {
                 1
             } else {
                 -1
@@ -115,42 +95,27 @@ impl Renderer {
         };
         let mut err = dx + dy;
 
-        let mut x = a[0];
-        let mut y = a[1];
+        let mut x = a.0;
+        let mut y = a.1;
 
         loop {
-            self.draw_pixel([x, y], WHITE);
+            if x > 0 && y > 0 {
+                self.draw_pixel((x as usize, y as usize), WHITE);
+            }
 
-            /*
-            This first checks if the row 'y' has an initialised vector of pixels. If the vector
-            has already been initialised, the pixel is inserted in order using the binary search matcher.
-            */
-            // match row_pixels.entry(y) {
-            //     Entry::Vacant(e) => {
-            //         e.insert(vec![x]);
-            //     }
-            //     Entry::Occupied(mut e) => {
-            //         let vec = e.get_mut();
-            //         match vec.binary_search(&x) {
-            //             Ok(_) => {}
-            //             Err(pos) => e.get_mut().insert(pos, x),
-            //         }
-            //     }
-            // }
-
-            if x == b[0] && y == b[1] {
+            if x == b.0 && y == b.1 {
                 break;
             }
             let err2 = err * 2;
             if err2 >= dy {
-                if x == b[0] {
+                if x == b.0 {
                     break;
                 }
                 err = err + dy;
                 x = x + sx;
             }
             if err2 <= dx {
-                if y == b[1] {
+                if y == b.1 {
                     break;
                 }
                 err = err + dx;
@@ -159,14 +124,15 @@ impl Renderer {
         }
     }
 
-    pub fn draw_pixel(&mut self, pos: [i64; 2], col: u32) {
-        if pos[0] < 0 || pos[0] > self.width as i64 - 1 {
+    pub fn draw_pixel(&mut self, pos: (usize, usize), col: u32) {
+        if pos.0 > self.width - 1 {
             return;
         }
-        if pos[1] < 0 || pos[1] > self.height as i64 - 1 {
+        if pos.1 > self.height - 1 {
             return;
         }
-        let i = (self.width * pos[1] as usize) + pos[0] as usize;
+
+        let i = (self.width * pos.1) + pos.0;
         if i < self.buffer.len() {
             self.buffer[i] = col;
         }
@@ -174,11 +140,5 @@ impl Renderer {
 
     pub fn clear(&mut self) {
         self.buffer = vec![BLACK; self.width * self.height];
-    }
-
-    fn draw_sky(&mut self) {
-        self.buffer = vec![SKY; (self.width * self.height) / 2];
-        self.buffer
-            .append(&mut vec![GROUND; ((self.width * self.height) / 2)]);
     }
 }
